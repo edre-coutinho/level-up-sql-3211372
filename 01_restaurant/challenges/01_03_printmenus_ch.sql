9a726ae2ee116a678f8907c434a2de437c48b5b1
edre-coutinho <113918188+edre-coutinho@users.noreply.github.com>
2023-03-09 18:24:33 +0000

    solution 1-6

--- 01_restaurant/challenges/01_03_printmenus_ch.sql
+++ 01_restaurant/challenges/01_03_printmenus_ch.sql
@@ -1,5 +1,27 @@
 -- Create reports that will be used to make three menus.
 
 -- Create a report with all the items sorted by price (lowest to highest).
+SELECT
+  type, name, price, description
+FROM
+  dishes
+ORDER BY 
+  price ASC;
 -- Create a report showing appetizers and beverages.
+SELECT
+  type, name, price, description
+FROM
+  dishes
+WHERE
+  type = 'Appetizer' OR type = 'Beverage'
+ORDER BY 
+  type;
 -- Create a report with all items except beverages.
+SELECT
+  type, name, price, description
+FROM
+  dishes
+WHERE
+  type != 'Beverage'
+ORDER BY 
+  type ASC;
--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -3,3 +3,8 @@
 -- A customer named Norby has notified us he won't 
 -- be able to keep his Friday reservation. 
 -- Today is July 24, 2022.
+SELECT *
+FROM Reservations r JOIN Customers c ON r.CustomerID = c.CustomerID 
+WHERE FirstName = 'Norby' AND Date >= '2022-07-24';
+
+DELETE FROM Reservations WHERE ReservationID = 2000;
