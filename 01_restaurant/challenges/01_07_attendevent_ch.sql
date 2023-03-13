e524e6d1d3a3086a7b4af706a031eb51c91587e2
edre-coutinho <113918188+edre-coutinho@users.noreply.github.com>
2023-03-13 18:37:05 +0000

    q7

--- 01_restaurant/challenges/01_07_attendevent_ch.sql
+++ 01_restaurant/challenges/01_07_attendevent_ch.sql
@@ -2,3 +2,10 @@
 
 -- The customer 'atapley2j@kinetecoinc.com' will be in
 -- attendance, and will bring 3 friends.
+INSERT INTO Attendees (CustomerID, PartySize)
+VALUES (
+(SELECT CustomerID FROM Customers WHERE Email = 'atapley2j@kinetecoinc.com'),
+3
+);
+
+SELECT * FROM Attendees;
