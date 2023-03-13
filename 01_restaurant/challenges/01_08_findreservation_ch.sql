782d42e4018bc854feb37f3c827b7c255a697788
edre-coutinho <113918188+edre-coutinho@users.noreply.github.com>
2023-03-13 22:02:04 +0000

    q8, q9 e q10

--- 01_restaurant/challenges/01_08_findreservation_ch.sql
+++ 01_restaurant/challenges/01_08_findreservation_ch.sql
@@ -3,5 +3,8 @@
 
 -- Variations of the name include:
 -- Stevensen, Stephensen, Stevenson, Stephenson, Stuyvesant
-
+-- St%s%
 -- There are four people in the party. Today is June 14th.
+SELECT c.LastName, c.FirstName, r.Date, r.PartySize
+FROM Customers c JOIN Reservations r ON c.CustomerID = r.CustomerID
+WHERE PartySize = 4 AND LastName LIKE 'St%s%' AND Date LIKE '%-06-14%'
--- 01_restaurant/challenges/01_10_takeorder_ch.sql
+++ 01_restaurant/challenges/01_10_takeorder_ch.sql
@@ -7,3 +7,29 @@
 -- 1 Tropical Blue Smoothie
 -- Delivery date and time: September 20, 2022 @ 2PM (14:00)
 -- There are no taxes or other fees.
+
+INSERT INTO Orders (CustomerID, OrderDate)
+VALUES (
+  (SELECT CustomerID 
+  FROM Customers 
+  WHERE FirstName = 'Loretta' AND LastName = 'Hundey' AND Address = '6939 Elka Place'),
+  '2022-09-20 14:00:00'
+);
+SELECT * FROM Orders WHERE OrderDate = '2022-09-20 14:00:00';
+
+INSERT INTO OrdersDishes (OrderID, DishID)
+VALUES
+  (1001,
+  (SELECT DishID FROM Dishes WHERE Name = 'House Salad')),
+  (1001,
+  (SELECT DishID FROM Dishes WHERE Name = 'Mini Cheeseburgers')),
+  (1001,
+  (SELECT DishID FROM Dishes WHERE Name = 'Tropical Blue Smoothie'));
+SELECT * FROM OrdersDishes WHERE OrderID = 1001;
+
+SELECT SUM(d.Price) FROM OrdersDishes od JOIN Dishes d ON od.DishID = d.DishID WHERE OrderID = 1001;
+
+-- just to make sure
+SELECT od.DishID, d.Name, d.Price FROM OrdersDishes od JOIN Dishes d ON od.DishID = d.DishID WHERE OrderID = 1001;
+
+
