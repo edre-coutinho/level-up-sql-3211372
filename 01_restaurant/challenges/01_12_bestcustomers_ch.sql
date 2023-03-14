4f962925142c588cc6197d9977386a24ce26c4ba
edre-coutinho <113918188+edre-coutinho@users.noreply.github.com>
2023-03-14 14:04:51 +0000

    q11 e q12

--- 01_restaurant/challenges/01_12_bestcustomers_ch.sql
+++ 01_restaurant/challenges/01_12_bestcustomers_ch.sql
@@ -1,3 +1,11 @@
 -- Identify a few customers who have ordered delivery
 -- from the restaurant the most often, so we can send
 -- them a promotional coupon.
+WITH loyal AS (
+  SELECT COUNT(OrderID) AS Count, CustomerID 
+  FROM Orders 
+  GROUP BY CustomerID 
+)
+SELECT c.FirstName, c.LastName, c.Email, l.Count 
+FROM Customers c JOIN loyal l ON c.CustomerID = l.CustomerID 
+ORDER BY Count DESC
