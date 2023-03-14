8918eb9c17e38c4b9635ce82dbcf1b677b2f6905
edre-coutinho <113918188+edre-coutinho@users.noreply.github.com>
2023-03-14 21:54:34 +0000

    Q4, Q5, Q6 library

--- 02_library/challenges/02_04_booksdue_ch.sql
+++ 02_library/challenges/02_04_booksdue_ch.sql
@@ -2,3 +2,9 @@
 -- to the library on July 13, 2022.
 -- Provide the due date, the book title, and
 -- the borrower's first name and email address.
+SELECT p.FirstName, p.LastName, p.Email, b.Title, l.DueDate 
+FROM Patrons p 
+JOIN Loans l ON p.PatronID = l.PatronID
+JOIN Books b ON l.BookID = b.BookID
+WHERE ReturnedDate IS NULL
+AND DueDate = '2022-07-13'
--- 02_library/challenges/02_06_visitreminder_ch.sql
+++ 02_library/challenges/02_06_visitreminder_ch.sql
@@ -1,2 +1,7 @@
 -- Prepare a report of the library patrons
 -- who have checked out the fewest books.
+SELECT p.FirstName, p.LastName, p.Email, COUNT(l.LoanID) AS LoanCount
+FROM Patrons p JOIN Loans l ON p.PatronID = l.PatronID
+GROUP BY l.PatronID
+ORDER BY LoanCount
+LIMIT 5
