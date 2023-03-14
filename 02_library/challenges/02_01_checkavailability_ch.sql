d8d1bf87ce32c0e3ef2d910d4de3d1796c56f526
edre-coutinho <113918188+edre-coutinho@users.noreply.github.com>
2023-03-14 17:55:08 +0000

    Q1, Q2, Q3 library

--- 02_library/challenges/02_01_checkavailability_ch.sql
+++ 02_library/challenges/02_01_checkavailability_ch.sql
@@ -1,2 +1,18 @@
 -- Determine how many copies of the book 'Dracula'
 -- are available for library patrons to borrow.
+SELECT COUNT(Title) AS NumberOfCopies 
+FROM Books 
+WHERE Title = 'Dracula';
+--
+SELECT COUNT(b.Title) AS NumberOfBooks 
+FROM Loans l JOIN Books b ON l.BookID = b.BookID
+WHERE Title = 'Dracula' AND ReturnedDate IS NULL;
+--
+SELECT
+  (SELECT COUNT(Title) AS NumberOfCopies 
+  FROM Books 
+  WHERE Title = 'Dracula') -
+  (SELECT COUNT(b.Title) AS NumberOfBooks 
+  FROM Loans l JOIN Books b ON l.BookID = b.BookID
+  WHERE Title = 'Dracula' AND ReturnedDate IS NULL)
+  AS Available_Copies;
